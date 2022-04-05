db94cfc2921a66b4d3589f994bfbaacccbf4e34a
Adriel Tosi <adriel.tosi@netcentric.biz>
2022-04-05 10:23:29 +0200

    basic setup

---
+++ poke/src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Nav from '../components/Nav/Nav.svelte';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<Nav />
+<slot />
+
+<style global lang="scss">
+	@import '../styles/global.scss';
+</style>
---
+++ poke/src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Poke from '../components/icons/Poke.svelte';
+	import Paragraph from '../components/Paragraph/Paragraph.svelte';
+</script>
+
+<Poke />
+<Paragraph
+	content={'Hi there! This is POKE, a "stupid Pokemon list" that represents complex and complete Pokemons, allowing them to shine on local stages and beyond.'}
+/>
